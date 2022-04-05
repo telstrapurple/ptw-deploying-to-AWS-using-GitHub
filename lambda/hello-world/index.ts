af0a3056483c8f96962f8d88dbeb815705c515c4
Trent Jones <trent.jones@purple.telstra.com>
2022-04-05 09:52:35 +0800

    Simplify response body

--- lambda/hello-world/index.ts
+++ lambda/hello-world/index.ts
@@ -3,8 +3,6 @@ import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello, World!",
-    }),
+    body: "Hello, World!",
   };
 };
